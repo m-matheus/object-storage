1569ba6b2459e934b09893a83250e152c77cdd9b
Matheus Moraes <116031835+m-matheus@users.noreply.github.com>
2026-06-01 20:16:47 -0300

    feat: add bruno tests and fix the global exception

--- src/main/java/com/matheus/object_storage/exception/GlobalExceptionHandler.java
+++ src/main/java/com/matheus/object_storage/exception/GlobalExceptionHandler.java
@@ -1,14 +1,14 @@
 package com.matheus.object_storage.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import com.matheus.object_storage.bucket.BucketAlreadyExistsException;
 import com.matheus.object_storage.bucket.BucketNotFoundException;
 import com.matheus.object_storage.object.ObjectNotFoundException;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BucketNotFoundException.class)
